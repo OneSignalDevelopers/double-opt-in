f11ffafc85b288d7ce4e6002a85a66ecbae2f2d9
William Shepherd <williamrshepherd@gmail.com>
2024-09-11 15:15:22 -0500

    Link to signup page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@
 import { DoubleOptInModal } from '../components/double-opt-in-modal'
 import { TextToSubscribeModal } from '../components//text-to-subscribe-modal'
 import { useState } from 'react'
+import Link from 'next/link'
 
 export default function HomePage() {
   const [textToSubscribeModalOpen, setTextToSubscribeModalOpen] =
@@ -11,6 +12,12 @@ export default function HomePage() {
 
   return (
     <div className="flex flex-col">
+      <Link
+        className="bg-[#051B2C] text-white font-medium py-3 px-6 hover:bg-opacity-90 transition duration-200"
+        href="/sign-up"
+      >
+        Sign-up Page
+      </Link>
       <button
         className="bg-[#051B2C] text-white font-medium py-3 px-6 hover:bg-opacity-90 transition duration-200"
         onClick={() => setTextToSubscribeModalOpen(true)}
