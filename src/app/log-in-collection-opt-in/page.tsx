b098cdfc1a4d126aa9b7074669113bbf704e08a8
alecharmon <alec@onesignal.com>
2024-09-17 10:19:37 -0400

    In order to use useSearchParams the componet needs to be wraped in a susptense element

--- src/app/log-in-collection-opt-in/page.tsx
+++ src/app/log-in-collection-opt-in/page.tsx
@@ -1,11 +1,21 @@
 'use client'
 
-import { FormEvent, useState } from 'react'
+import { FormEvent, useState, Suspense } from 'react'
 import { CountryCodesOptions, OneSignalAppID } from '@core/constants'
 import { safeTry } from '@core/utils'
 import { useSearchParams } from 'next/navigation'
 
 export default function SignUpPage() {
+  return (
+    <div>
+      <Suspense fallback={<div>Loading...</div>}>
+        <SignUpForm />
+      </Suspense>
+    </div>
+  );
+};
+
+function SignUpForm() {
   const searchParams = useSearchParams()
   const [formData, setFormData] = useState({
     name: '',
